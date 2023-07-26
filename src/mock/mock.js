47ad1eb5febe8f9dfd5e06fd4c79c6c4ef3212c4
郑埴 <1420223146@qq.com>
2023-07-26 20:54:39 +0800

    '修复模拟的图片不能正常显示的问题'

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,13 +1,13 @@
 import Mock from 'mockjs';
 
 Mock.mock('/asset/activities', 'get', {
-  'activities|10': [                                                    // 生成10个活动数据
+  'activities|10': [                                                            // 生成10个活动数据
     {
-      'act_name': '@ctitle(5, 10)',                                     // 随机生成中文标题，长度在5到10之间
-      'act_time': '@date("yyyy-MM-dd")',                                // 随机生成日期，格式为"yyyy-MM-dd"
-      'act_address': '@city(true)',                                     // 随机生成城市名
-      'act_participants': '@integer(50, 200)',                          // 随机生成参加人数，范围在50到200之间
-      'act_photoURL': '@image("200x150", "@color", "@ctitle(3, 5)")',   // 随机生成图片URL，尺寸为200x150，背景色为随机颜色，标题为3到5个中文字符
+      'act_name': '@ctitle(5, 10)',                                             // 随机生成中文标题，长度在5到10之间
+      'act_time': '@date("yyyy-MM-dd")',                                        // 随机生成日期，格式为"yyyy-MM-dd"
+      'act_address': '@city(true)',                                             // 随机生成城市名
+      'act_participants': '@integer(50, 200)',                                  // 随机生成参加人数，范围在50到200之间
+      'act_photoURL': '@image("200x150", "@color", @color, "@ctitle(3, 5)")',   // 随机生成图片URL，尺寸为200x150，背景色为随机颜色，标题为3到5个中文字符
     },
   ],
 });
